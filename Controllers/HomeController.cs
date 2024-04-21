d5cd4d20cf94a907563f3794a8c64208803a158c
AIRA FUGEN <airafugen14@gmail.com>
2024-04-21 13:40:02 +0800

    add the file change password

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace Onlinequiz.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
